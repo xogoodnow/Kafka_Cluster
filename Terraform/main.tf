00af736d67acae6e68ba05b7b230d2d097a09fbf
xogoodnow <xo4ever9999@gmail.com>
2024-01-05 15:59:59 +0330

    Added the main terraform runner

---
+++ Terraform/main.tf
@@ -0,0 +1,21 @@
+module "ssh_keys" {
+  source = "./modules/ssh_keys"
+  hcloud_token = var.hcloud_token
+}
+
+
+module "servers" {
+  source       = "./modules/servers"
+  hcloud_token = var.hcloud_token
+  image_name   = var.image_name
+  server_type  = var.server_type
+  location     = var.location
+  depends_on   = [module.ssh_keys]
+}
+
+
+
+module "Redis" {
+  source = "./modules/Kafka"
+  hcloud_token = var.hcloud_token
+}
